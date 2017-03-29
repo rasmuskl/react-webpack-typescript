b8f2e187a03fa993c7b59713d776a9cce597b00e
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2017-03-29 21:13:55 +0200

    Added Typescript code in demo. Adjusted presentation.

--- 20170331-microsoft-60min/01-typescript-react/app.tsx
+++ 20170331-microsoft-60min/01-typescript-react/app.tsx
@@ -2,9 +2,43 @@
 
 
 // Clicker
+class Clicker extends React.Component<void, { count: number }> {
+    constructor() {
+        super();
+        this.state = { count: 0 };
+    }
+
+    increment() {
+        this.setState({ count: this.state.count + 1 });
+    }
+
+    render() {
+        return (
+            <div onClick={() => this.increment()}>
+                {this.state.count}
+            </div>
+        );
+    }
+}
 
 
-// Counter
+// Counter 
+class Counter extends React.Component<void, { count: number }> {
+    constructor() {
+        super();
+        this.state = { count: 0 };
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({count: this.state.count + 1});
+        }, 1000);
+    }
+
+    render() {
+        return <div>{this.state.count}</div>;
+    }
+}
 
 let layout = (
     <div>Hello!</div>
